92c41510280825488ceb17a1f53a2ea59af657ac
Uduru0522 <uduru0522@gmail.com>
2023-05-24 12:37:49 +0900

    Completed calculation, adjusted learning rate

--- task05/main.cpp
+++ task05/main.cpp
@@ -30,6 +30,10 @@ void wdw_spring_3d(
 
   // write a few lines of code below to compute the gradient of elastic energy of this spring
   // with respect to the positions of the two end points.
+  const Eigen::Vector3f direction = (node2xyz[0] - node2xyz[1]) / length; // direction vector of the spring
+
+  dw[0] = stiffness * C * direction; // gradient for the first end point
+  dw[1] = -stiffness * C * direction; // gradient for the second end point
 }
 
 float gradient_descent_energy_minimization(
@@ -73,7 +77,7 @@ float gradient_descent_energy_minimization(
 }
 
 int main() {
-  constexpr float learning_rate = 6.5e-3f;
+  constexpr float learning_rate = 6.7e-3f;
   constexpr int num_theta = 64;
   const auto[tri2vtx, vtx2xyz_ini] = pba::generate_mesh_annulus3(0.3, 0.8, 32, num_theta);
   const auto line2vtx = pba::lines_of_mesh(tri2vtx, static_cast<int>(vtx2xyz_ini.rows()));
